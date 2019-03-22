767fdcffd810113d106f21060afaa972ef844597
Eszes Norbert <eszesnorbi@gmail.com>
2019-03-22 12:36:04 +0100

    listen 8082, mongodb init

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log('init')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+var db
+
+app.use(cors())
+app.use(bodyParser.json())
+
+MongoClient.connect('mongodb://localhost:27017/', {
+    useNewUrlParser: true
+}, (err, client) => {
+    if (err) throw err
+    db = client.db('beadando')
+})
+
+app.get('/',
+    (req, res) =>
+    res.send('Hello World!')
+)
+
+app.listen(8082)
